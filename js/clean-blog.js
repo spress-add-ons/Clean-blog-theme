a10e73e9b9736dcbe515ff1915d74a85f000c7e8
Yo! Symfony <yosymfony@gmail.com>
2015-01-25 09:46:38 +0000

    Minor changes

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,9 +1,9 @@
 ---
 ---
 /*!
- * Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2014 Start Bootstrap
- * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
+ * Clean blog theme for Spress v1.0.0 (http://yosymfony.com/)
+ * Copyright 2015 Victor Puertas
+ * Licensed under Apache 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
  */
 
 // Tooltip Init
